c19d58d909b54b43c1f9ef7e391af6a9c39c0dfb
MengTy199 <117325886+MengTy199@users.noreply.github.com>
2024-04-17 17:35:42 +0700

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <home/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Home from './views/Home.vue'
 
-nav {
-  padding: 30px;
-}
+  export default{
+  components: { Home },
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  }
+</script>
---
+++ src/components/Education.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Education
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
